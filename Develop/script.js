702b51278bbf369a2c5f8098dc0e4111b54dfaa1
Justin B <jbedford94@gmail.com>
2021-07-11 19:03:42 -0400

    added in confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,29 @@
-// Password parameters
+// Password charachter possibillities 
+var Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharachters = ["!","?", ".", "/"];
+var Upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var Lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// Password distinctions
+var Length= "";
+var choseUpper;
+var choseLower;
+var choseNumbers;
+var ChoseSpecial;
+
+//8-127 charachters
 function generatePassword() {
-  var length = 8,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-      retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
+  var Length = prompt("How long is your password? (choose between 8-127)");
+
+  while(Length <=8 || length >=127) {
+    alert("Invalid response! Retry");
+    var Length = prompt("How long is your password? (choose between 8-127)");
   }
-  return retVal;
+  var choseUpper = confirm("Click Ok to include upper case letters");
+  var choseLower = ("Click Ok to include lower case letters");
+  var choseNumbers = ("Click Ok to include Numbers");
+  var ChoseSpecial = ("Click Ok to include special charachters");
+
 }
 
 // Get references to the #generate element
@@ -24,24 +40,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-document.querySelector('#generate').addEventListener('click', promptpassword);
-
-function promptpassword() {
-    var Passlength = prompt("Length of password? (between 8-128 characters)");
-    console.log("" + passlength)
-    alert (Passlength);
-}
-
-
-
-
-/*
-
-function getRandomLower() {
-  return String.fromCharCode(math.floor(Math.random() * 26) +97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(math.floor(Math.random() * 26) +65);
-}*/
