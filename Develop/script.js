ede73e47c655a20e9113bcab6deb8a7595852c3b
Justin B <jbedford94@gmail.com>
2021-07-11 18:23:22 -0400

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
-// Assignment code here
+// Password parameters
 
+function generatePassword() {
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +24,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+document.querySelector('#generate').addEventListener('click', promptpassword);
+
+function promptpassword() {
+    var Passlength = prompt("Length of password? (between 8-128 characters)");
+    console.log("" + passlength)
+    alert (Passlength);
+}
+
+
+
+
+/*
+
+function getRandomLower() {
+  return String.fromCharCode(math.floor(Math.random() * 26) +97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(math.floor(Math.random() * 26) +65);
+}*/
